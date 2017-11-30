198d2e6d624a7fb496b5ceea60d970038a7ed6d4
qi <qizhang@tvunetworks.com>
2017-11-30 09:55:29 +0800

    init HelloOpenGL

---
+++ HelloOpenGL/HelloOpenGL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloOpenGL
+//
+//  Created by qi on 30/11/2017.
+//  Copyright © 2017 tvu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
